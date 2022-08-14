46779190cffef4822b92329cde4e39968fd4a25f
dunghenry <trandungksnb6930082001@gmail.com>
2022-08-14 16:46:46 +0700

    test action mutations getters

--- components/TodoList.vue
+++ components/TodoList.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
     <h1>TodoList Component</h1>
+    <AddTodo/>
+    <div v-for="todo in todos" :key="todo.id">
+        <TodoItem :todo="todo" />
+    </div>
+    <TodoItem />
   </div>
 </template>
 
 <script>
+import TodoItem from './TodoItem.vue';
+import AddTodo from './AddTodo.vue';
 export default {
-
+  props: ['todos'],
+  components: { TodoItem, AddTodo }
 }
 </script>
 
--- pages/todos/index.vue
+++ pages/todos/index.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
      <h1>Todos Page</h1>
+     <TodoList :todos="todos"/>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex';
+import TodoList from '../../components/TodoList.vue';
 export default {
-
+  created() {
+    this.setTodos();
+  },
+  computed: {...mapState("todos", ["todos"])},
+  methods: { ...mapActions("todos", ["setTodos"]) },
+  components: { TodoList }
 }
 </script>
 
